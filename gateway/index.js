635eb72ba515c76081d71708c1af208cf5f4fdd0
ColoradaGreis <graciana.baratti@gmail.com>
2023-07-26 20:05:26 -0300

    gateway

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const morgan = require("morgan")
+
+const app = express()
+app.use(morgan("dev"))
+
+app.use("/characters", createProxyMiddleware({
+    target: "http://localhost:8001", //dónde va a estar escuchando la app
+    changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
+}))
+
+app.listen(8000, () => {
+    console.log("gateway on port 8000")
+})
