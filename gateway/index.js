5e14037a9fa67e9c9397520c74de4bde25812453
ColoradaGreis <graciana.baratti@gmail.com>
2023-07-30 22:28:21 -0300

    gateway

--- gateway/index.js
+++ gateway/index.js
@@ -9,6 +9,14 @@ app.use("/characters", createProxyMiddleware({
     target: "http://localhost:8001", //dónde va a estar escuchando la app
     changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
 }))
+app.use("/films", createProxyMiddleware({
+    target: "http://localhost:8002", //dónde va a estar escuchando la app
+    changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
+}))
+app.use("/planets", createProxyMiddleware({
+    target: "http://localhost:8003", //dónde va a estar escuchando la app
+    changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
+}))
 
 app.listen(8000, () => {
     console.log("gateway on port 8000")
