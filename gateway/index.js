05e60eb9479ccbfc947f5ab05f108f2acfd1e978
ColoradaGreis <graciana.baratti@gmail.com>
2023-08-01 18:45:40 -0300

    docker network gateway-characters

--- gateway/index.js
+++ gateway/index.js
@@ -6,15 +6,15 @@ const app = express()
 app.use(morgan("dev"))
 
 app.use("/characters", createProxyMiddleware({
-    target: "http://localhost:8001", //dónde va a estar escuchando la app
+    target: "http://characters:8001", //dónde va a estar escuchando la app, que es el puerto 8001 del contenedor charaacters
     changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
 }))
 app.use("/films", createProxyMiddleware({
-    target: "http://localhost:8002", //dónde va a estar escuchando la app
+    target: "http://films:8002", //dónde va a estar escuchando la app
     changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
 }))
 app.use("/planets", createProxyMiddleware({
-    target: "http://localhost:8003", //dónde va a estar escuchando la app
+    target: "http://planets:8003", //dónde va a estar escuchando la app
     changeOrigin: true, //indica que a partir de ahí se van a modificar los headers
 }))
 
